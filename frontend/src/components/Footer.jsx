961efeb4210875cb783a9f5079390eb77686802b
MystereFire <48494738+MystereFire@users.noreply.github.com>
2025-08-10 21:58:13 +0200

    Remove auto margin from footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer({ className = '' }) {
   return (
     <footer
-      className={`bg-white text-center text-gray-500 p-4 mt-auto border-t ${className}`}
+      className={`bg-white text-center text-gray-500 p-4 border-t ${className}`}
     >
       &copy; {new Date().getFullYear()} CarCare
     </footer>
