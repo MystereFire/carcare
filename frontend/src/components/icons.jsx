d742452bda1f28abf0cee34386020c7439add4c9
MystereFire <48494738+MystereFire@users.noreply.github.com>
2025-08-19 22:01:19 +0200

    Remove lucide-react dependency and add local icons

---
+++ frontend/src/components/icons.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export const Plus = (props) => (
+  <span {...props}>+</span>
+);
+
+export const Pencil = (props) => (
+  <span {...props}>✏️</span>
+);
+
+export const Wrench = (props) => (
+  <span {...props}>🔧</span>
+);
+
+export const Gauge = (props) => (
+  <span {...props}>🧭</span>
+);
+
+export const Euro = (props) => (
+  <span {...props}>€</span>
+);
+
+export const Droplet = (props) => (
+  <span {...props}>💧</span>
+);
+
+export const PiggyBank = (props) => (
+  <span {...props}>🐖</span>
+);
+
+export default {
+  Plus,
+  Pencil,
+  Wrench,
+  Gauge,
+  Euro,
+  Droplet,
+  PiggyBank
+};
